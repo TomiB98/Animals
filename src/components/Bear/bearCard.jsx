0f5ce78a1885db5411ef9dd060f814686ad18185
TomiB98 <tomasbaldi@gmail.com>
2024-08-05 09:59:03 -0300

    Styles + function

--- src/components/Bear/bearCard.jsx
+++ src/components/Bear/bearCard.jsx
@@ -1,16 +1,27 @@
+import { motion, AnimatePresence } from 'framer-motion';
+
 import style from './bear.module.css'
 
 function Bear() {
     return (
-            <div className={style.bearCardCont}>
-                <div className={style.text}>
-                    <h1>B E A R S</h1>
-                    <h2>Polar Bear: <br/> An impresive size</h2>
-                    <p>Is one of the most emblematic victims of global warming. The melting
-                        of the Arctic is reducing its hunting ground.</p>
-                    <button className={style.btnDonate}>DONATE NOW</button>
+        <AnimatePresence>
+            <motion.div className={style.bearCard}
+                initial={{ scaleX: 0.23, originX: 1 }}
+                animate={{ scaleX: 1, originX: 1 }}
+                transition={{ duration: 0.5 }} >
+
+                <div className={style.bearCardCont}>
+                    <div className={style.text}>
+                        <h1>B E A R S</h1>
+                        <h2>Polar Bear: <br /> An impresive size</h2>
+                        <p>Is one of the most emblematic victims of global warming. The melting
+                            of the Arctic is reducing its hunting ground.</p>
+                        <button className={style.btnDonate}>DONATE NOW</button>
+                    </div>
                 </div>
-            </div>
+                
+            </motion.div>
+        </AnimatePresence>
     )
 };
 
