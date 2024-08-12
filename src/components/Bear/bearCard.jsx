fa33be4c295f7a36612d296f38e58f31e6dc6131
TomiB98 <tomasbaldi@gmail.com>
2024-08-12 11:13:16 -0300

    Functions

--- src/components/Bear/bearCard.jsx
+++ src/components/Bear/bearCard.jsx
@@ -2,27 +2,36 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 import style from './bear.module.css'
 
-function Bear() {
+function Bear({ load, animal, prevAnimal }) {
     return (
         <AnimatePresence>
-            <motion.div className={style.bearCard}
-                initial={{ scaleX: 0.4, originX: 1 }}
-                animate={{ scaleX: 1, originX: 1 }}
-                transition={{ duration: 0.8 }} >
+            {(animal === 'bear' || prevAnimal === 'bear') &&
+                <motion.div className={style.bearCard} style={animal !== 'bear' && { position: 'absolute', top: 0, right: 0 }}
+                    initial={load ? false : (animal === 'bear' ? { scaleX: 0.4, originX: 1 } : { scaleX: 1.02, originX: 1 })}
+                    animate={load ? false : (animal === 'bear' ? { scaleX: 1, originX: 1 } : { scaleX: 0.423, originX: 1, opacity: 0 })}
+                    transition={{ duration: 0.4 }} >
 
-                <div className={style.bearCardCont}>
+                    <motion.div className={style.bearCardCont}
+                        initial={load ? false : (animal === 'bear' ? { scaleX: 0.5, scaleY: 0.5, x: 120, y: 45 } : { scaleX: 1, scaleY: 1, x: 0, y: 0 })}
+                        animate={load ? false : (animal === 'bear' ? { scaleX: 1, scaleY: 1, x: 0, y: 0 } : { scaleX: 0.5, scaleY: 0.5, x: 120, y: 45 })}
+                        transition={{ duration: 0.4 }}>
 
-                    <div className={style.text}>
-                        <h1>B E A R S</h1>
-                        <h2>Polar Bear: <br /> An impresive size</h2>
-                        <p>Is one of the most emblematic victims of global warming. The melting
-                            of the Arctic is reducing its hunting ground.</p>
-                        <button className={style.btnDonate}>DONATE NOW</button>
-                    </div>
-                    
-                </div>
-                
-            </motion.div>
+                        <motion.div className={style.text}
+                            initial={load ? false : { opacity: 0.2 }}
+                            animate={load ? false : { opacity: 1 }}
+                            transition={{ duration: 0.4, delay: 0 }}>
+
+                            <h1>B E A R S</h1>
+                            <h2>Polar Bear: <br /> An impresive size</h2>
+                            <p>Is one of the most emblematic victims of global warming. The melting
+                                of the Arctic is reducing its hunting ground.</p>
+                            <button className={style.btnDonate}>DONATE NOW</button>
+                        </motion.div>
+
+                    </motion.div>
+
+                </motion.div>
+            }
         </AnimatePresence>
     )
 };
