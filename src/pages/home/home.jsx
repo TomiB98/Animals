5c17cbb968da3eb19ea9f99cdbb74f84e6c39b53
TomiB98 <tomasbaldi@gmail.com>
2024-08-15 10:35:44 -0300

    Style + Function

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,38 @@
-import style from './home.module.css'
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
 import Bear from '../../components/Bear/bearCard'
-import Rinho from '../../components/Rinho/rinhoCard'
+import Rhino from '../../components/Rinho/rinhoCard'
 import Tiger from '../../components/Tiger/tigerCard'
-import React, { useState } from 'react';
+
+import imgTiger from '../../asets/Image=1.png'
+import imgRhino from '../../asets/Image=2.png'
+import imgBear from '../../asets/Image=3.png'
+
+import style from './home.module.css'
 
 function Home() {
 
-    // ###Estados y funciones para oscurecer contenedor cuando estoy sobre el boton de cambio animales### //
+    // ###Estados y funciones animales### //
     const [hoveredButton, setHoveredButton] = useState(null);
     const [animal, setAnimal] = useState('rhino');
+    const [prevAnimal, setPrevAnimal] = useState('rhino');
+    const [initialLoad, setInitialLoad] = useState(true);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setInitialLoad(false);
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 649);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const handleMouseEnter = (index) => {
         setHoveredButton(index);
@@ -19,76 +43,200 @@ function Home() {
     };
 
     const handleAnimalShow = (index) => {
+        setPrevAnimal(animal);
         setAnimal(index);
     };
 
+    const backgroundImages = {
+        rhino: `url(${imgRhino})`,
+        tiger: `url(${imgTiger})`,
+        bear: `url(${imgBear})`
+    };
+
     return (
-        <div className={style.home}>
 
-            {/* ####Card and button Tiger#### */}
+        <div className={style.home} style={{ backgroundImage: !isMobile && backgroundImages[animal] }}>
 
-            {animal === 'tiger' ? null :
-                <div className={style.second} style={{ backgroundColor: hoveredButton === 0 ? 'rgba(0, 0, 0, 0.350)' : 'rgba(0, 0, 0, 0.171)' }}>
+            {!isMobile && (
+                <AnimatePresence>
+                    {/* ####Button Tiger efect Rhino => Tiger#### */}
+                    {animal !== 'tiger' && (
+                        <motion.div style={{ display: 'contents' }}>
 
-                    <button className={style.btnAnimals} onClick={() => handleAnimalShow('tiger')} onMouseEnter={() => handleMouseEnter(0)} onMouseLeave={handleMouseLeave}>
-                        <div className={style.name}>TIGERS</div>
-                        <div className={style.plus}>
-                            <svg width="50" height="50" viewBox="0 0 50 50">
-                                <line x1="20" y1="10" x2="20" y2="40" stroke="rgb(112, 237, 253)" stroke-width="1" stroke-dasharray="2 1" />
-                                <line x1="4" y1="25" x2="40" y2="25" stroke="rgb(112, 237, 253)" stroke-width="1" stroke-dasharray="2 1" />
-                            </svg>
-                        </div>
-                    </button>
+                            <motion.div className={style.btnCont} style={{ backgroundColor: hoveredButton === 0 ? '#00000045' : '#00000035' }}
+                                onClick={() => { handleAnimalShow('tiger') }} onMouseEnter={() => handleMouseEnter(0)} onMouseLeave={handleMouseLeave}
+                                initial={initialLoad ? false : { opacity: 0 }}
+                                animate={initialLoad ? false : { opacity: 1 }}
+                                transition={{ duration: 0.7 }}>
 
-                </div>
-            }
+                                <button className={`${style.btnAnimals} ${hoveredButton === 0 ? style.hovered : ''}`} >
+                                    <div className={style.name}>TIGERS</div>
+                                    <div className={style.plus}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 40 40" fill="none">
+                                            <path d="M1.25 20H38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                            <path d="M20 1.25V38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                        </svg>
+                                    </div>
+                                </button>
 
-            {animal === 'tiger' ? <div className={style.first}><Tiger /></div> : null}
-            {/* <div className={style.first}><Tiger /></div> */}
+                            </motion.div>
 
-            {/* ####Card and button Rhino#### */}
+                            <Tiger animal={animal} prevAnimal={prevAnimal} load={initialLoad} />
 
-            {animal === 'rhino' ? null :
-                <div className={style.second} style={{ backgroundColor: hoveredButton === 1 ? 'rgba(0, 0, 0, 0.350)' : 'rgba(0, 0, 0, 0.171)' }}>
+                        </motion.div>
+                    )}
 
-                    <button className={style.btnAnimals} onClick={() => handleAnimalShow('rhino')} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
-                        <div className={style.name}>RHINOCEROS</div>
-                        <div className={style.plus}>
-                            <svg width="50" height="50" viewBox="0 0 50 50">
-                                <line x1="20" y1="10" x2="20" y2="40" stroke="rgb(112, 237, 253)" stroke-width="1" stroke-dasharray="2 1" />
-                                <line x1="4" y1="25" x2="40" y2="25" stroke="rgb(112, 237, 253)" stroke-width="1" stroke-dasharray="2 1" />
-                            </svg>
-                        </div>
-                    </button>
 
-                </div>
-            }
+                    {/* ####Button Rhinocero efect Tiger => Bear#### */}
+                    {animal === 'bear' && prevAnimal === 'tiger' && (
+                        <motion.div className={style.btnCont} style={{ backgroundColor: hoveredButton === 1 ? '#00000045' : '#00000035' }}
+                            initial={initialLoad ? false : { scaleX: 1.01, originX: -142 }}
+                            animate={initialLoad ? false : { scaleX: 1, originX: -141 }}
+                            transition={{ duration: 0.3 }}>
 
-            {animal === 'rhino' ? <div className={style.first}><Rinho /></div> : null}
-            {/* <div className={style.first}><Rinho /></div> */}
+                            <button className={style.btnAnimals} onClick={() => handleAnimalShow('rhino')} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
+                                <div className={style.name}>RHINOCEROS</div>
+                                <div className={style.plus}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 40 40" fill="none">
+                                        <path d="M1.25 20H38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                        <path d="M20 1.25V38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                    </svg>
+                                </div>
+                            </button>
 
-            {/* ####Card and button Bear#### */}
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            )}
 
-            {animal === 'bear' ? null :
-                <div className={style.second} style={{ backgroundColor: hoveredButton === 2 ? 'rgba(0, 0, 0, 0.350)' : 'rgba(0, 0, 0, 0.171)' }}>
 
-                    <button className={style.btnAnimals} onClick={() => handleAnimalShow('bear')} onMouseEnter={() => handleMouseEnter(2)} onMouseLeave={handleMouseLeave}>
-                        <div className={style.name}>BEARS</div>
-                        <div className={style.plus}>
-                            <svg width="50" height="50" viewBox="0 0 50 50">
-                                <line x1="20" y1="10" x2="20" y2="40" stroke="rgb(112, 237, 253)" stroke-width="1" stroke-dasharray="2 1" />
-                                <line x1="4" y1="25" x2="40" y2="25" stroke="rgb(112, 237, 253)" stroke-width="1" stroke-dasharray="2 1" />
-                            </svg>
-                        </div>
-                    </button>
+            {/* ####Card Tiger#### */}
+            {!isMobile && animal === 'tiger' && (<Tiger animal={animal} prevAnimal={prevAnimal} load={initialLoad} />)}
+            {isMobile && (<Tiger animal={'tiger'} load={initialLoad} />)}
 
-                </div>
-            }
 
-            {animal === 'bear' ? <div className={style.first}><Bear /></div> : null}
-            {/* <div className={style.first}><Bear /></div> */}
-        </div>
+            {!isMobile && (
+                <AnimatePresence>
+                    {/* ####Button Rhinocero efect Rhino => Bear#### */}
+                    {animal === 'bear' && prevAnimal === 'rhino' && (
+                        <motion.div style={{ display: 'contents' }}>
 
+                            <motion.div className={style.btnCont} style={{ backgroundColor: hoveredButton === 1 ? '#00000045' : '#00000035' }}
+                                initial={initialLoad ? false : { opacity: 0 }}
+                                animate={initialLoad ? false : { opacity: 1 }}
+                                transition={{ duration: 0.7 }}>
+
+                                <button className={style.btnAnimals} onClick={() => handleAnimalShow('rhino')} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
+                                    <div className={style.name}>RHINOCEROS</div>
+                                    <div className={style.plus}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 40 40" fill="none">
+                                            <path d="M1.25 20H38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                            <path d="M20 1.25V38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                        </svg>
+                                    </div>
+                                </button>
+
+                            </motion.div>
+
+                            <Rhino animal={animal} prevAnimal={prevAnimal} load={initialLoad} />
+
+                        </motion.div>
+                    )}
+
+
+                    {/* ####Button Rhinocero efect Rhino => Tiger#### */}
+                    {animal === 'tiger' && prevAnimal === 'rhino' && (
+
+                        <motion.div style={{ display: 'contents' }}>
+
+                            <Rhino animal={animal} prevAnimal={prevAnimal} load={initialLoad} />
+
+                            <motion.div className={style.btnCont} style={{ backgroundColor: hoveredButton === 1 ? '#00000045' : '#00000035' }}
+                                initial={initialLoad ? false : { opacity: 0 }}
+                                animate={initialLoad ? false : { opacity: 1 }}
+                                transition={{ duration: 0.7 }}>
+
+                                <button className={style.btnAnimals} onClick={() => handleAnimalShow('rhino')} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
+                                    <div className={style.name}>RHINOCEROS</div>
+                                    <div className={style.plus}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 40 40" fill="none">
+                                            <path d="M1.25 20H38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                            <path d="M20 1.25V38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                        </svg>
+                                    </div>
+                                </button>
+
+                            </motion.div>
+
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            )}
+
+
+            {/* ####Card Rhino#### */}
+            {!isMobile && animal === 'rhino' && (<Rhino animal={animal} prevAnimal={prevAnimal} load={initialLoad} />)}
+            {isMobile && (<Rhino animal={'rhino'} prevAnimal={'rhino'} />)}
+
+
+            {!isMobile && (
+                <AnimatePresence>
+                    {/* ####Button Rhinocero efect Bear => Tiger#### */}
+                    {animal === 'tiger' && prevAnimal === 'bear' && (
+                        <motion.div className={style.btnCont} style={{ backgroundColor: hoveredButton === 1 ? '#00000045' : '#00000035' }}
+                            initial={initialLoad ? false : ({ scaleX: 1.01, originX: 142 })}
+                            animate={initialLoad ? false : ({ scaleX: 1, originX: 141 })}
+                            transition={{ duration: 0.3 }}>
+
+                            <button className={style.btnAnimals} onClick={() => handleAnimalShow('rhino')} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={handleMouseLeave}>
+                                <div className={style.name}>RHINOCEROS</div>
+                                <div className={style.plus}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 40 40" fill="none">
+                                        <path d="M1.25 20H38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                        <path d="M20 1.25V38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                    </svg>
+                                </div>
+                            </button>
+
+                        </motion.div>
+                    )}
+
+
+                    {/* ####Button Bear efect Bear => Rhino#### */}
+                    {animal !== 'bear' && (
+                        <motion.div style={{ display: 'contents' }}>
+
+                            <Bear animal={animal} prevAnimal={prevAnimal} load={initialLoad} />
+
+                            <motion.div className={style.btnCont} style={{ backgroundColor: hoveredButton === 2 ? '#00000045' : '#00000035' }}
+                                initial={initialLoad ? false : { opacity: 0 }}
+                                animate={initialLoad ? false : { opacity: 1 }}
+                                transition={{ duration: 0.7 }}>
+
+                                <button className={style.btnAnimals} onClick={() => { handleAnimalShow('bear') }} onMouseEnter={() => handleMouseEnter(2)} onMouseLeave={handleMouseLeave}>
+                                    <div className={style.name}>BEARS</div>
+                                    <div className={style.plus}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 40 40" fill="none">
+                                            <path d="M1.25 20H38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                            <path d="M20 1.25V38.75" stroke="#70FFE5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="3 3" />
+                                        </svg>
+                                    </div>
+                                </button>
+
+                            </motion.div>
+
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            )}
+
+
+            {/* ####Card Bear#### */}
+            {!isMobile && animal === 'bear' && (<Bear animal={animal} prevAnimal={prevAnimal} load={initialLoad} />)}
+            {isMobile && (<Bear animal={'bear'} load={initialLoad} />)}
+
+
+        </div >
     )
 };
 
