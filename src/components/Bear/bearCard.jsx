2dae93f35257f1fee0377f800ecf51202b8f9d6a
TomiB98 <tomasbaldi@gmail.com>
2024-08-28 16:26:48 -0300

    Functions

--- src/components/Bear/bearCard.jsx
+++ src/components/Bear/bearCard.jsx
@@ -7,25 +7,26 @@ function Bear({ load, animal, prevAnimal }) {
         <AnimatePresence>
             {(animal === 'bear' || prevAnimal === 'bear') &&
                 <motion.div className={style.bearCard} style={animal !== 'bear' && { position: 'absolute', top: 0, right: 0 }}
-                    initial={load ? false : (animal === 'bear' ? { scaleX: 0.4, originX: 1 } : { scaleX: 1.02, originX: 1 })}
-                    animate={load ? false : (animal === 'bear' ? { scaleX: 1, originX: 1 } : { scaleX: 0.423, originX: 1, opacity: 0 })}
-                    transition={{ duration: 0.4 }} >
+                    initial={load ? false : (animal === 'bear' ? { scaleX: 0.499, originX: 1 } : { scaleX: 0.997, originX: 1 })}
+                    animate={load ? false : (animal === 'bear' ? { scaleX: 1, originX: 1 } : { scaleX: 0.499, originX: 1, opacity: 0, display: 'none' })}
+                    transition={{ duration: 0.3 }} >
 
                     <motion.div className={style.bearCardCont}
-                        initial={load ? false : (animal === 'bear' ? { scaleX: 0.5, scaleY: 0.5, x: 120, y: 45 } : { scaleX: 1, scaleY: 1, x: 0, y: 0 })}
-                        animate={load ? false : (animal === 'bear' ? { scaleX: 1, scaleY: 1, x: 0, y: 0 } : { scaleX: 0.5, scaleY: 0.5, x: 120, y: 45 })}
-                        transition={{ duration: 0.4 }}>
+                        initial={load ? false : (animal === 'bear' ? { scaleX: 0.35, scaleY: 0.5, x: 110, y: 45 } : { scaleX: 1, scaleY: 1, x: 0, y: 0 })}
+                        animate={load ? false : (animal === 'bear' ? { scaleX: 1, scaleY: 1, x: 0, y: 0 } : { scaleX: 0.4, scaleY: 0.5, x: 120, y: 45 })}
+                        transition={{ duration: 0.3 }}>
 
                         <motion.div className={style.text}
                             initial={load ? false : { opacity: 0.2 }}
                             animate={load ? false : { opacity: 1 }}
-                            transition={{ duration: 0.4, delay: 0 }}>
+                            transition={{ duration: 0.3 }}>
 
                             <h1>B E A R S</h1>
                             <h2>Polar Bear: <br /> An impresive size</h2>
                             <p>Is one of the most emblematic victims of global warming. The melting
                                 of the Arctic is reducing its hunting ground.</p>
                             <button className={style.btnDonate}>DONATE NOW</button>
+                            
                         </motion.div>
 
                     </motion.div>
